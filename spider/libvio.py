4f1d64b57ffbbdc58486f95ec22846a9ad0b07cd
sec-an <sec_an@foxmail.com>
2022-08-31 00:01:14 +0800

    Update libvio.py

--- spider/libvio.py
+++ spider/libvio.py
@@ -154,8 +154,7 @@ def playerContent(ids, flag, token):
         allScript = BeautifulSoup(requests.get(url=url, headers=getHeaders(url)).text, "html.parser").select("script")
         for item in allScript:
             scContent = item.get_text().strip()
-            if scContent.startswith("var "
-                                    ""):
+            if scContent.startswith("var player_"):
                 player = json.loads(scContent[scContent.find('{'):scContent.rfind('}') + 1])
                 if player.get("from") in playerConfig:
                     pCfg = playerConfig.get(player.get("from"))
